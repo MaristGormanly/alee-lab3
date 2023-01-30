fba4ac77aad24e6607d094a4ab8ac65efd10bba8
alee0724 <112659408+alee0724@users.noreply.github.com>
2023-01-30 11:04:20 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require ('http');
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {
+    'Content-Type': 'text-plain'
+  });
+  reponse.end('Hello World\n');
+}).listen(1337);
+console.log('Server running on port 1337!');
