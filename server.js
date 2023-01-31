18c031163a21f0021b119a3bb0fbc0efadd6a525
alee0724 <112659408+alee0724@users.noreply.github.com>
2023-01-31 08:17:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 })
 
 app.get('/', function (req, res) {
-  res.send('GET request to homepage')
+  res.send('/page1')
 })
 
 app.listen(1337, () => console.log('Marist Chatter listening on port 1337!'));
